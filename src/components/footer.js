335316f7bad3e4bac531474761d196d4527b06ef
Max Li <maxlivinci@gmail.com>
2018-04-11 15:22:33 +0800

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,52 +1,56 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import Link from 'next/link'
+
 import IconButton from './icon-button'
 import NavButton from '../components/nav-button'
 
 const Wrap = styled.footer`
   flex: 1;
   display: flex;
-  flex-direction: column;
+  justify-content: flex-end;
+  max-width: 80%;
+  margin: 1.5rem auto;
 `
 
 const InnerBlock = styled.div`
-  margin: ${props => props.style.margin};
   display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <Wrap>
-        <InnerBlock style={{ margin: '10px' }}>
-          <NavButton>
-            <a>
-              <IconButton
-                style={{
-                  image: '../../static/imgs/github.svg',
-                  width: '20px',
-                  height: '20px'
-                }}
-              />
-            </a>
-          </NavButton>
-          <NavButton>
-            <a>
-              <IconButton
-                style={{
-                  image: '../../static/imgs/linkedin.svg',
-                  width: '20px',
-                  height: '20px'
-                }}
-              />
-            </a>
-          </NavButton>
-        </InnerBlock>
-        <InnerBlock style={{ margin: '0 0 10px 20px' }}>
-          Polo Max © 2018
-        </InnerBlock>
-      </Wrap>
-    )
-  }
-}
+const Licence = styled.div`
+  font-size: 0.9rem;
+`
+
+const Footer = () => (
+  <Wrap>
+    <InnerBlock>
+      <Licence>Polo Max © 2018</Licence>
+      <NavButton style={{ padding: '0.5rem' }}>
+        <a>
+          <IconButton
+            style={{
+              image: '../../static/imgs/github.svg',
+              width: '1rem',
+              height: '1rem'
+            }}
+          />
+        </a>
+      </NavButton>
+      <NavButton style={{ padding: 0 }}>
+        <a>
+          <IconButton
+            style={{
+              image: '../../static/imgs/linkedin.svg',
+              width: '1rem',
+              height: '1rem'
+            }}
+          />
+        </a>
+      </NavButton>
+    </InnerBlock>
+  </Wrap>
+)
+
+export default Footer
--- src/components/nav-button.js
+++ src/components/nav-button.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const Wrap = styled.div`
+const NavButton = styled.div`
   padding: 10px 20px;
   font-size: 1.4rem;
 `
 
-export default class NavButton extends Component {
-  render() {
-    return <Wrap>{this.props.children}</Wrap>
-  }
-}
+export default NavButton
