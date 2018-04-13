812d64f994df1ec31732934f85ab14aebfe5695b
線上讀書會 <polo13999@gmail.com>
2018-04-13 23:30:37 +0800

    background done

--- src/app/src/components/sketch.js
+++ src/app/src/components/sketch.js
@@ -1,41 +1,72 @@
-let distance = 30
-let colorLib = [
-  '#ccafd9',
-  '#ffc2e4',
-  '#FCD8AC',
-  '#C0F1F9',
-  '#BDE6C7',
-  '#CCAFD9'
-]
-const setbg = p => {
-  p.background(255, 255, 255)
-  //p.style("z-index", "-1")
-  //count total point
-  //p.windowWidth  p.windowHeight
-  let maxX = Math.floor(p.windowWidth / 30)
-  let maxY = Math.floor(p.windowHeight / 30)
-
-  p.noStroke()
-  let c = '#ccafd9'
-  for (let i = 0; i < maxX; i++) {
-    for (let j = 0; j < maxY; j++) {
-      c = p.color(colorLib[Math.floor(Math.random() * 5)])
-      p.fill(c) // Use color variable 'c' as fill color
-      p.ellipse(i * distance, j * distance, 3)
-    }
-  }
-}
+var e = [],
+  n = [
+    'rgba(109,224,242,1)',
+    'rgba(69, 194, 100, 1)',
+    'rgba(249,168,54,1)',
+    'rgba(255,114,196,1)',
+    'rgba(139,64,169,1)'
+  ]
+var y = 100
 
 const sketch = p => {
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight)
-    setbg(p)
+  p.getRandomInt = (t, e) => {
+    return Math.floor(Math.random() * (e - t + 1)) + t
   }
+  p.setup = () => {
+    let canvas = p.createCanvas(p.windowWidth, p.windowHeight)
+    p.stroke(255) // Set line drawing color to white
+    p.frameRate(30)
+    p.noLoop()
+    for (var t = 0; t < p.windowWidth; t += 50) {
+      for (var i = 0; i < p.windowHeight; i += 50) {
+        var a = n[p.getRandomInt(0, 4)]
+        e.push({ x: t, y: i, dotColour: a })
+        p.beginShape()
+        p.ellipse(t, i, 4, 4)
+        p.noStroke()
+        p.endShape()
+      }
+    }
+    document.addEventListener('mousemove', p.reDraw)
+  }
+  p.draw = () => {
+    for (var t = 0, n = 0; n < p.windowWidth; n += 50) {
+      for (var i = 0; i < p.windowHeight; i += 50) {
+        if (e[t]) {
+          p.beginShape()
+          p.fill(e[t].dotColour)
+          p.noStroke()
+          p.ellipse(n, i, 4, 4)
+          p.endShape()
+          t++
+        }
+      }
+    }
+  }
+  p.reDraw = t => {
+    p.clear()
+    //console.log(t.clientY)
+    //console.log(e)
+    p.beginShape()
+    let result = 0
+    for (var n = 0; n < e.length; n++) {
+      result = Math.sqrt(
+        (t.clientX - e[n].x) * (t.clientX - e[n].x) +
+          (t.clientY - e[n].y) * (t.clientY - e[n].y)
+      )
+      if (result < 80) {
+        // console.log('result')
+        // console.log(result)
+        p.stroke(e[n].dotColour)
+        p.line(e[n].x, e[n].y, t.clientX, t.clientY)
+        p.endShape()
+      }
+    }
+    p.draw()
+  }
+
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
-    setbg(p)
   }
-  p.draw = () => {}
 }
-
 export default sketch
