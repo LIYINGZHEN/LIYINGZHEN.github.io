713134001aeceb382e539110be096ff7d0bc31be
線上讀書會 <polo13999@gmail.com>
2018-04-13 15:17:59 +0800

    make the bg point done

--- src/app/src/components/sketch.js
+++ src/app/src/components/sketch.js
@@ -1,10 +1,41 @@
-const sketch = p => {
-  p.setup = () => p.createCanvas(p.windowWidth, p.windowHeight)
-  p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight)
+let distance = 30
+let colorLib = [
+  '#ccafd9',
+  '#ffc2e4',
+  '#FCD8AC',
+  '#C0F1F9',
+  '#BDE6C7',
+  '#CCAFD9'
+]
+const setbg = p => {
+  p.background(255, 255, 255)
+  //p.style("z-index", "-1")
+  //count total point
+  //p.windowWidth  p.windowHeight
+  let maxX = Math.floor(p.windowWidth / 30)
+  let maxY = Math.floor(p.windowHeight / 30)
 
-  p.draw = () => {
-    p.background(255, 255, 255)
+  p.noStroke()
+  let c = '#ccafd9'
+  for (let i = 0; i < maxX; i++) {
+    for (let j = 0; j < maxY; j++) {
+      c = p.color(colorLib[Math.floor(Math.random() * 5)])
+      p.fill(c) // Use color variable 'c' as fill color
+      p.ellipse(i * distance, j * distance, 3)
+    }
   }
+}
+
+const sketch = p => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+    setbg(p)
+  }
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
+    setbg(p)
+  }
+  p.draw = () => {}
 }
 
 export default sketch
