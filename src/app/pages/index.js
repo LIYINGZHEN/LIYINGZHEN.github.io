7f7815cd3e208f2347414b388756ea5ee43cc5d8
Max Li <maxlivinci@gmail.com>
2018-04-15 14:01:57 +0800

    fix duplications

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -33,6 +33,71 @@ const A = styled.a`
   }
 `
 
+const images = [
+  {
+    img: 'https://jgog.in/app/301233416e364674c1306b37cdb189fe-1000.png',
+    title: 'Biketinerary',
+    category: 'Product Design',
+    description: 'Explore a city using its bikeshare network.'
+  },
+  {
+    img: 'https://jgog.in/app/57786d174bc78b9324b142228303a115-600.jpg',
+    title: 'Central Park Conservancy',
+    category: 'Central Park Conservancy',
+    description: 'An intensive study to find the painpoints on the CPC website.'
+  },
+  {
+    img: 'https://jgog.in/app/49c81bb12bf3c703634015e31d81812e-600.jpg',
+    title: 'Reactive Particles',
+    category: 'Interaction Design',
+    description: 'A fun interactive installation for the SVA studio'
+  },
+  {
+    img: 'https://jgog.in/app/d263cbad5d8aae6728b753f0b10bd18c-600.png',
+    title: 'Super Co-Mario',
+    category: 'Physical Computing',
+    description: 'An experiment to play Super Mario without a controller.'
+  },
+  {
+    img: 'https://jgog.in/app/3d25747b2317b4e57b23dfdb09722afc-800.png',
+    title: 'Bill Splitter',
+    category: 'Product Design',
+    description:
+      'Making it easier to split the bill and tip with multiple people.'
+  },
+  {
+    img: 'https://jgog.in/app/3b439bd6b6433e2015f7f46aaff8f475-600.png',
+    title: 'Mark43',
+    category: 'Product Design • Design System',
+    description: 'A story of fighting crime with design at my internship.'
+  },
+  {
+    img: 'https://jgog.in/app/a81e2fefb16b3af58cdc884f78eaacc7-600.jpg',
+    title: 'Cita',
+    category: 'Concept Design',
+    description: 'A chatbot to make plans for your friend circle.'
+  },
+  {
+    img: 'https://jgog.in/app/e4e024fcef6150e0e5c2a9adb70a0271-600.jpg',
+    title: 'Cosine',
+    category: 'Physical Computing',
+    description: 'A lighting installation.'
+  }
+]
+
+const mapImg = images =>
+  images.map(({ img, title, category, description }) => (
+    <Card
+      key={title}
+      theme={{
+        img,
+        title,
+        category,
+        description
+      }}
+    />
+  ))
+
 const Index = () => (
   <div>
     <Hero />
@@ -46,86 +111,8 @@ const Index = () => (
     <br />
     <Container theme={{ marginTop: '2rem', width: '90%' }}>
       <Row>
-        <Col>
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/301233416e364674c1306b37cdb189fe-1000.png',
-              title: 'Biketinerary',
-              category: 'Product Design',
-              description: 'Explore a city using its bikeshare network.'
-            }}
-          />
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/57786d174bc78b9324b142228303a115-600.jpg',
-              title: 'Central Park Conservancy',
-              category: 'Central Park Conservancy',
-              description:
-                'An intensive study to find the painpoints on the CPC website.'
-            }}
-          />
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/49c81bb12bf3c703634015e31d81812e-600.jpg',
-              title: 'Reactive Particles',
-              category: 'Interaction Design',
-              description: 'A fun interactive installation for the SVA studio'
-            }}
-          />
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/d263cbad5d8aae6728b753f0b10bd18c-600.png',
-              title: 'Super Co-Mario',
-              category: 'Physical Computing',
-              description:
-                'An experiment to play Super Mario without a controller.'
-            }}
-          />
-        </Col>
-        <Col>
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/3d25747b2317b4e57b23dfdb09722afc-800.png',
-              title: 'Bill Splitter',
-              category: 'Product Design',
-              description:
-                'Making it easier to split the bill and tip with multiple people.'
-            }}
-          />
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/3b439bd6b6433e2015f7f46aaff8f475-600.png',
-              title: 'Mark43',
-              category: 'Product Design • Design System',
-              description:
-                'A story of fighting crime with design at my internship.'
-            }}
-          />
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/a81e2fefb16b3af58cdc884f78eaacc7-600.jpg',
-              title: 'Cita',
-              category: 'Concept Design',
-              description: 'A chatbot to make plans for your friend circle.'
-            }}
-          />
-          <Card
-            theme={{
-              img:
-                'https://jgog.in/app/e4e024fcef6150e0e5c2a9adb70a0271-600.jpg',
-              title: 'Cosine',
-              category: 'Physical Computing',
-              description: 'A lighting installation.'
-            }}
-          />
-        </Col>
+        <Col>{mapImg(images.slice(0, 4))}</Col>
+        <Col>{mapImg(images.slice(4, 8))}</Col>
       </Row>
     </Container>
     <P>
