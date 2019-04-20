74341ca572defc7205c505205a64c88fd5aa56e2
Max Li <maxlivinci@gmail.com>
2019-04-20 07:53:51 +0200

    script: add publish script

---
+++ publish.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+DIR=$(dirname "$0")
+
+echo "Deleting old publication"
+rm -rf public
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B master public origin/master
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
+git push origin master
+cd ..
+rm -rf public
